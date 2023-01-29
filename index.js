0d0dfc5a16915f9c380b9afed8b99a92a6042168
AHADKHAN2202 <ahad.khan7999782202@gmail.com>
2023-01-29 23:26:44 +0530

    added more features

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const error_msg = document.querySelector('.errors span');
 const time_msg = document.querySelector('.time span');
 const WPM_msg = document.querySelector('.WPM span');
 const CPM_msg = document.querySelector('.CPM span');
-const NWPM_msg = document.querySelector('NWPM span');
-const ACCURACY_msg = document.querySelector('.ACCURACY span')
+// const NWPM_msg = document.querySelector('NWPM span');
+const ACCURACY_msg = document.querySelector('.ACCURACY span');
+
+// const select_input = document.querySelector('#select');
 const button = document.querySelector('button');
 
 let letterIndex = 0;
@@ -14,30 +16,35 @@ let timer;
 let maxTime = 60;
 let timeLeft = maxTime;
 let isTyping = 0;
+let RandomIndex = 0;
 
-
-function RandomPara() {
+function RandomPara() {  // to get random paragraph
 
     // console.log(paragraph.);
 
-    let RandomIndex = Math.floor(Math.random() * paragraph.length);
-
-    //    console.log(RandomIndex);
-
+    RandomIndex = Math.floor(Math.random() * paragraph.length); // getting random index of the paragraph
+    
     writing_text.innerHTML = "";
 
-    paragraph[RandomIndex].split("").forEach((span) => {
+    // console.log(writing_text);
 
-        // console.log(span.length);
+    paragraph[RandomIndex].split("").forEach((char) => {
 
-        let spanTag = `<span>${span}</span>`;
+        let spanTag = `<span>${char}</span>`;
 
         writing_text.innerHTML += spanTag;
-        console.log(spanTag.length);
+
+        // let spanTag = document.createElement('span');
+        //   spanTag.innerText = span;
+        //   writing_text.appendChild(spanTag);
+
+       
+        
+        // console.log(spanTag.length);
     });
 
     writing_text.querySelectorAll('span')[0].classList.add('active');
-
+      
     document.addEventListener('keydown', () => input_area.focus());
 
     writing_text.addEventListener('click', () => input_area.focus());
@@ -52,9 +59,11 @@ function initTyping() {
 
     const Letters = writing_text.querySelectorAll('span');
 
-    // console.log(letters); 
+    // console.log(Letters); 
 
     let typedLetter = input_area.value.split('')[letterIndex];
+
+    // console.log(characterTyped);
 
     if (letterIndex < Letters.length-1 && timeLeft > 0) {
 
@@ -94,11 +103,19 @@ function initTyping() {
         error_msg.innerText = errors;
 
         CPM_msg.innerText = letterIndex - errors; // cpm not count errors
+
         let Wpm = Math.round((((letterIndex - errors) / 5) / (maxTime - timeLeft)) * 60);
     
         Wpm = Wpm < 0 || !Wpm || Wpm === Infinity ? 0 : Wpm;
 
         WPM_msg.innerText = Wpm;
+
+        let correctCharacters = CPM_msg.innerText;
+        let totalKey_pressed = paragraph[RandomIndex].split('').length;
+        
+      let  accuracy = Math.round((correctCharacters/totalKey_pressed)*100);
+    //   console.log(accuracy);
+      ACCURACY_msg.innerText = accuracy;
 
     } else {
         input_area.value = "";
@@ -128,7 +145,54 @@ function resetTyping(){
     error_msg.innerText = errors;
     WPM_msg.innerText = 0;
     CPM_msg.innerText = 0;
-
+    error_msg.innerText = 0;
+    ACCURACY_msg.innerText = 0;
 }
 
 button.addEventListener('click',resetTyping)
+
+
+
+// select part js
+
+
+const select = document.getElementById("select");
+// const prior = document.getElementsByClassName("option")[select].value;
+
+select.addEventListener('change',displayMsg);
+
+function displayMsg(e){
+//  if(e.classList.contains('option')){
+    console.log(e.target.value);
+    if(e.target.value==='easy'){
+        writing_text.innerHTML = "";
+
+        function RandomPara() {  // to get random paragraph
+
+    // console.log(paragraph.);
+
+    RandomIndex = Math.floor(Math.random() * easy.length);
+    
+    writing_text.innerHTML = "";
+
+    easy[RandomIndex].split('').forEach((span) => {
+
+        let spanTag = `<span>${span}</span>`;
+
+        writing_text.innerHTML += spanTag;
+        
+        // console.log(spanTag.length);
+    });
+
+    writing_text.querySelectorAll('span')[0].classList.add('active');
+      
+    document.addEventListener('keydown', () => input_area.focus());
+
+    writing_text.addEventListener('click', () => input_area.focus());
+
+}
+
+RandomPara();
+    }
+//  }
+}
